98b6b3e82882ae66b8136ba07f2573ecee07fbe2
Jaskaran Singh <262jaskaran@gmail.com>
2022-01-24 18:15:23 +0530

    Time: 4 ms (76.74%), Space: 9.4 MB (78.67%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -4,14 +4,36 @@ public:
         int m=matrix.size();
         int n=matrix[0].size();
         
-        for(int i=0;i<m;i++){
-            if(matrix[i][n-1]>=target){
-                for(int j=n-1;j>=0;j--){
-                    if(matrix[i][j]==target) return true;
-                }
-                break;
-            }
+        // for(int i=0;i<m;i++){
+        //     if(matrix[i][n-1]>=target){
+        //         for(int j=n-1;j>=0;j--){
+        //             if(matrix[i][j]==target) return true;
+        //         }
+        //         break;
+        //     }
+        // }
+        // return false;
+        
+        //ceiling using binary search
+        int lo=0,hi=m-1;
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(matrix[mid][n-1]>=target) hi=mid-1;
+            else lo=mid+1;
         }
+        
+        int row = lo;
+        if(row>m-1) row=m-1;
+        lo=0,hi=n-1;
+        
+        //simple binary search
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(matrix[row][mid] == target) return true;
+            else if(matrix[row][mid]<target) lo=mid+1;
+            else hi=mid-1;
+        }
+        
         return false;
     }
 };
