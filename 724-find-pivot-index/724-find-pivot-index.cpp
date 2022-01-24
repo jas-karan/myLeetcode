97931b90a231730c782a9989a553a3874cc982e6
Jaskaran Singh <262jaskaran@gmail.com>
2022-01-24 18:51:10 +0530

    Time: 20 ms (89.63%), Space: 31.1 MB (63.97%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -2,14 +2,27 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n = nums.size();
-        int pre[n], suf[n];
+//         int pre[n], suf[n];
         
-        pre[0] = 0; suf[n-1] = 0;
+//         pre[0] = 0; suf[n-1] = 0;
         
-        for(int i=1;i<n;i++) pre[i]=pre[i-1]+nums[i-1]; //left element + left of left element
-        for(int i=n-2;i>=0;i--) suf[i]=suf[i+1]+nums[i+1]; 
+//         for(int i=1;i<n;i++) pre[i]=pre[i-1]+nums[i-1]; //left element + left of left element
+//         for(int i=n-2;i>=0;i--) suf[i]=suf[i+1]+nums[i+1]; 
         
-        for(int i=0;i<n;i++) if(pre[i]==suf[i]) return i;
+//         for(int i=0;i<n;i++) if(pre[i]==suf[i]) return i;
+        
+//         return -1;
+        
+        int lsum = 0, rsum = 0;
+        
+        for(int i=n-1;i>0;i--) rsum+=nums[i]; // for first element
+        
+        for(int i=0;i<n;i++){
+            if(lsum == rsum) return i;
+            lsum+=nums[i];
+            if(i<n-1)rsum-=nums[i+1];
+            else rsum=0;   //make it zero for last element
+        }
         
         return -1;
     }
