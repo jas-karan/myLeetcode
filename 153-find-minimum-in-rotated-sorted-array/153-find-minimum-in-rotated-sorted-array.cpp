78a1a2da22e51ff1956f51005241d51c43ac3ec1
Jaskaran Singh <262jaskaran@gmail.com>
2022-01-26 15:16:36 +0530

    Time: 9 ms (13.86%), Space: 10.3 MB (21.47%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,10 +1,26 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]<nums[i-1]) return nums[i];
+//         for(int i=1;i<nums.size();i++){
+//             if(nums[i]<nums[i-1]) return nums[i];
+//         }
+        
+//         return nums[0];  //array is not rotated
+        
+        //------ binary Search ----
+        //finding pivot element - ele smaller than neighbors
+        
+        int n = nums.size();
+        if(nums[0]<=nums[n-1]) return nums[0]; //if not rotated
+        
+        int l=0,h=n-1;
+        while(l<h){
+            int m=(l+h)/2;
+            if(nums[m]>=nums[0]) l=m+1;
+            else h=m;
         }
         
-        return nums[0];
+        return nums[l];
+        
     }
 };
