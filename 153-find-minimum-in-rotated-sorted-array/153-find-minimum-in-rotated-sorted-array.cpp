75e013aae5137e2af0f1bf0f58aa4a9d1255fa75
Jaskaran Singh <262jaskaran@gmail.com>
2022-01-26 15:31:22 +0530

    Time: 5 ms (29.13%), Space: 10.1 MB (71.32%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -14,15 +14,11 @@ public:
         if(nums[0]<=nums[n-1]) return nums[0]; //if not rotated
         
         int l=0,h=n-1;
-        while(l<=h){
+        while(l<h){
             int m=(l+h)/2;
             
-            //if arr is rotated, then m+1 always exist
-            if(nums[m]>nums[m+1]) return nums[m+1];
-            else if(nums[m]<nums[m-1]) return nums[m];
-            else if(nums[m]>nums[0]) l=m+1;
-            else h=m-1;
-            
+            if(nums[m]>=nums[h]) l=m+1; //left is sorted
+            else h=m;
         }
         
         return nums[l];
