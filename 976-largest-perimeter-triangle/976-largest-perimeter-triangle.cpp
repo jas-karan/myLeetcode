6e0977a713e9a7c17b048551579c229ad7e62f26
Jaskaran Singh <262jaskaran@gmail.com>
2022-01-30 18:12:32 +0530

    Time: 63 ms (20.67%), Space: 21.9 MB (71.43%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.cpp
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.cpp
@@ -5,6 +5,8 @@ public:
         sort(nums.begin(),nums.end());
         
         for(int i=n-1;i>=2;i--){
+            //nums[i-1] and nums[i-2] are two largest elements before nums[i]
+            //if their sum exceeds nums[i] then any combo will make triangle with nums[i]
             if(nums[i]<nums[i-1]+nums[i-2]){
                 return nums[i]+nums[i-1]+nums[i-2];
             }
