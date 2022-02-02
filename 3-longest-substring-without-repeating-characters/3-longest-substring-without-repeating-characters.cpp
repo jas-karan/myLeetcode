b87085145a1a093ff5bd7a2d377921fe5f06ae6d
Jaskaran Singh <262jaskaran@gmail.com>
2022-02-02 15:51:33 +0530

    Time: 25 ms (46.28%), Space: 8.4 MB (57.16%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -5,20 +5,35 @@ public:
         //once window has an element with freq>1
         //shrink the window and update the length
         
-        unordered_map<int,int>window;
+//         unordered_map<int,int>window;
+//         int length=0;
+        
+//         int l=0,r=0;
+        
+//         while(r<s.length()){
+//             window[s[r]]++;
+            
+//             while(l<r && window[s[r]]>1){
+//                 window[s[l]]--;
+//                 l++;
+//             }
+//             r++;
+//             length = max(length,r-l);
+//         }
+        
+//         return length;
+        
+        unordered_map<char,int>mp;
         int length=0;
         
-        int l=0,r=0;
+        int l=0;
         
-        while(r<s.length()){
-            window[s[r]]++;
-            
-            while(l<r && window[s[r]]>1){
-                window[s[l]]--;
-                l++;
+        for(int r=0;r<s.length();r++){
+            if(mp.find(s[r]) != mp.end()){
+                l = max(l,mp[s[r]]+1);
             }
-            r++;
-            length = max(length,r-l);
+            length = max(length,r-l+1);
+            mp[s[r]]=r;
         }
         
         return length;
