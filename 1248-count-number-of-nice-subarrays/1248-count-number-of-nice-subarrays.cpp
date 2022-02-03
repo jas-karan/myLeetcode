a628e8e38f98d8e1647d6b15e2ff1ba4c648aea3
Jaskaran Singh <262jaskaran@gmail.com>
2022-02-03 18:38:08 +0530

    Time: 395 ms (11.79%), Space: 90.4 MB (27.44%) - LeetHub

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -0,0 +1,51 @@
+
+class Solution {
+public:
+//     int atmost(vector<int>nums, int k){
+//         int count=0;
+        
+//         int l=0,r=0;
+        
+//     }
+    
+    int numberOfSubarrays(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         int count=0;
+        
+//         for(int i=k-1;i<n;i++){
+//             int odd = 0;
+//             for(int j=i;j>=0;j--){
+//                 if(nums[j]%2!=0) odd++;
+                
+//                 if(odd==k) count++;
+//                 else if(odd>k) break;
+//             }
+//         }
+        
+//         return count;
+        
+        //time limit exceeded
+        
+//-------replace even numbers with 0 and odd with 1 then it is same as finding
+//subarrays with sum k
+        
+        unordered_map<int,int>mp;
+        
+        int sum = 0,count=0;
+        
+        for(int i:nums){
+            if(i%2==1) sum++;
+            
+            if(sum==k) count++;
+            if(mp.find(sum-k)!=mp.end()) count+=mp[sum-k];
+            mp[sum]++;
+        }
+        
+        return count;
+        
+//-------------------important-----------------
+        //finding exactly = k is atmost k minus atmost (k-1)
+        
+        
+    }
+};
