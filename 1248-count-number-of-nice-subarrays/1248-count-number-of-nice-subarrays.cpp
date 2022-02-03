4d4c35329e3bd51ff7a2695849988bc650f9f736
Jaskaran Singh <262jaskaran@gmail.com>
2022-02-03 18:42:49 +0530

    Time: 257 ms (30.23%), Space: 73.9 MB (34.01%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,12 +1,28 @@
 
 class Solution {
 public:
-//     int atmost(vector<int>nums, int k){
-//         int count=0;
+    int atmost(vector<int>nums, int k){
+        int count=0;
         
-//         int l=0,r=0;
+        int l=0,r=0;
         
-//     }
+        //no need of hashmap, we can simply count odds
+        int odd=0;
+        while(r<nums.size()){
+            if(nums[r]%2) odd++;
+            
+            while(l<=r && odd>k){
+                if(nums[l]%2) odd--;
+                l++;
+            }
+            
+            count+=(r-l+1);
+            r++;
+        }
+        
+        return count;
+        
+    }
     
     int numberOfSubarrays(vector<int>& nums, int k) {
 //         int n = nums.size();
@@ -29,23 +45,23 @@ public:
 //-------replace even numbers with 0 and odd with 1 then it is same as finding
 //subarrays with sum k
         
-        unordered_map<int,int>mp;
+//         unordered_map<int,int>mp;
         
-        int sum = 0,count=0;
+//         int sum = 0,count=0;
         
-        for(int i:nums){
-            if(i%2==1) sum++;
+//         for(int i:nums){
+//             if(i%2==1) sum++;
             
-            if(sum==k) count++;
-            if(mp.find(sum-k)!=mp.end()) count+=mp[sum-k];
-            mp[sum]++;
-        }
+//             if(sum==k) count++;
+//             if(mp.find(sum-k)!=mp.end()) count+=mp[sum-k];
+//             mp[sum]++;
+//         }
         
-        return count;
+//         return count;
         
 //-------------------important-----------------
         //finding exactly = k is atmost k minus atmost (k-1)
-        
+        return atmost(nums,k)-atmost(nums,k-1);
         
     }
 };
