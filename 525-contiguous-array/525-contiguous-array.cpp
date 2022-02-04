cfc4521cfe6ee9ff6ed25e4058b397a73e35f584
Jaskaran Singh <262jaskaran@gmail.com>
2022-02-04 18:23:20 +0530

    Time: 233 ms (24.05%), Space: 83.9 MB (55.07%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -3,15 +3,21 @@ public:
     int findMaxLength(vector<int>& nums) {
         //convert all 0's to -1's then 
         //just find largest zero sum subarray
+        //bcoz equal 1's and -1's means zero sum
         
         int length=0;
         int sum=0;
         unordered_map<int,int>mp;
+        
         for(int i=0;i<nums.size();i++){
+            
             sum+=(nums[i]==0)?-1:1;
+            
             if(sum==0) length=i+1;
             if(mp.find(sum)!=mp.end()) length=max(length,i-mp[sum]);
             if(mp.find(sum)==mp.end()) mp[sum]=i;
+            //only update if not present
+            //it will keep it largest
         }
         
         return length;
