0ab4cae691e98135a60591db3aed00252dde415c
Jaskaran Singh <262jaskaran@gmail.com>
2022-02-05 15:52:21 +0530

    Time: 62 ms (36.32%), Space: 21.6 MB (64.44%) - LeetHub

--- 554-brick-wall/554-brick-wall.cpp
+++ 554-brick-wall/554-brick-wall.cpp
@@ -2,10 +2,15 @@ class Solution {
 public:
     int leastBricks(vector<vector<int>>& wall) {
         
+        //basically we have to choose a coordinate which has max edges
+        //for a row keep adding brick sizes to find coordinates;
+        
         unordered_map<int,int>edge_cord;
         
         for(int i=0;i<wall.size();i++){
             int curredge = 0;
+            
+            //***** imp: dont consider last bridge ********
             for(int j=0;j<wall[i].size()-1;j++){
                 curredge+=wall[i][j];
                 edge_cord[curredge]++;
