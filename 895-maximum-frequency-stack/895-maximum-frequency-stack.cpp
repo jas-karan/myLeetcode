55fd5cd3fa9c9b1e3f6cd713ecd114376a3a7f7e
Jaskaran Singh <262jaskaran@gmail.com>
2022-02-05 16:20:33 +0530

    Time: 311 ms (42.01%), Space: 96.8 MB (15.13%) - LeetHub

---
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.cpp
@@ -0,0 +1,38 @@
+class FreqStack {
+public:
+    //we need to store num with its frequency
+    //we need to maintain maxfreq, to pop out
+    //the only problem is what is two ele has maxfreq => need nearest to top element!
+    //we can store ele with freq f in stack of that freq f
+    
+    unordered_map<int,int>freq;
+    unordered_map<int,stack<int>>stcks;
+    int maxFreq = INT_MIN;    
+    
+    FreqStack() {
+    }
+    
+    void push(int val) {
+        freq[val]++;
+        stcks[freq[val]].push(val);
+        maxFreq = max(maxFreq,freq[val]);
+    }
+    
+    int pop() {
+        int ele = stcks[maxFreq].top();
+        stcks[maxFreq].pop();
+        freq[ele]--;
+        if(stcks[maxFreq].size()==0){
+            stcks.erase(maxFreq);
+            maxFreq--;
+        }
+        return ele;
+    }
+};
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack* obj = new FreqStack();
+ * obj->push(val);
+ * int param_2 = obj->pop();
+ */
