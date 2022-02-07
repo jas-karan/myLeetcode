d80ab8425b49ec42a74d8ac0fc96b4fca19d0104
Jaskaran Singh <262jaskaran@gmail.com>
2022-02-07 19:50:35 +0530

    Time: 97 ms (10.02%), Space: 155.6 MB (7.79%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,19 +1,43 @@
 class Solution {
 public:
-    int helper(int present,int copied,int n){
+//     int helper(int present,int copied,int n){
+//         if(present>n) return INT_MAX;  //wrong operations
+//         if(present==n) return 0;  //exactly n are required
+        
+//         int copy_paste = helper(present+present,present,n);
+//         if(copy_paste!=INT_MAX) copy_paste+=2;   ///overflow
+        
+//         int paste = helper(present+copied,copied,n);
+//         if(paste!=INT_MAX) paste+=1;
+        
+//         return min(copy_paste,paste);
+//     }
+    
+    int helper(int present,int copied,int n,vector<vector<int>>&dp){
         if(present>n) return INT_MAX;  //wrong operations
         if(present==n) return 0;  //exactly n are required
         
-        int copy_paste = helper(present+present,present,n);
-        if(copy_paste!=INT_MAX) copy_paste+=2;
-        int paste = helper(present+copied,copied,n);
+        if(dp[present][copied]!=-1) return dp[present][copied];
+        
+        int copy_paste = helper(present+present,present,n,dp);
+        if(copy_paste!=INT_MAX) copy_paste+=2;   ///overflow
+        
+        int paste = helper(present+copied,copied,n,dp);
         if(paste!=INT_MAX) paste+=1;
         
-        return min(copy_paste,paste);
+        return dp[present][copied]=min(copy_paste,paste);
     }
     
     int minSteps(int n) {
+//         //we cannot copy only!! it will be infinite loop then,,keep copying...
+        
+//         if(n<2) return 0;
+//         return 1+helper(1,1,n);   //initially ,first op can be copy only
+        
+        //we can store overlapping subproblems
+        
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
         if(n<2) return 0;
-        return 1+helper(1,1,n);
+        return 1+helper(1,1,n,dp);
     }
 };
