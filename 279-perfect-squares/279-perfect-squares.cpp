7edb2d167019072afcd94a2ad5961d59406aef1a
Jaskaran Singh <262jaskaran@gmail.com>
2022-02-08 19:45:13 +0530

    Time: 259 ms (40.23%), Space: 9.1 MB (43.55%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     int numSquares(int n) {
+        //for every square of number <= n check if we include that square
+        //then how many more squares we have to take ie dp[n-square]
+        
+        //dp[i] = perfect numbers required for n=i
+        
         vector<int>dp(n+1,INT_MAX);
-        dp[0]=0;
+        
+        dp[0]=0; //0 perfect numbers are req for n=0
+        
         for(int i=1;i<=n;i++){
-            
-            for(int j=1;pow(j,2)<=i;j++){
-                dp[i]=min(dp[i],1+dp[i-pow(j,2)]);
+            for(int j=1;j*j<=i;j++){
+                
+                dp[i]=min(dp[i],1+dp[i-j*j]);
             }
         }
         
