57568f27d2b061039a077a9757d7d57b456ef0b4
Jaskaran Singh <262jaskaran@gmail.com>
2022-02-08 19:42:00 +0530

    Time: 235 ms (44.64%), Space: 9 MB (60.60%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int>dp(n+1,INT_MAX);
+        dp[0]=0;
+        for(int i=1;i<=n;i++){
+            
+            for(int j=1;pow(j,2)<=i;j++){
+                dp[i]=min(dp[i],1+dp[i-pow(j,2)]);
+            }
+        }
+        
+        return dp[n];
+    }
+};
