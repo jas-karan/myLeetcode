4c7aabd70a236f8035b9364a8fcb0530361ff95f
Jaskaran Singh <262jaskaran@gmail.com>
2022-02-11 18:17:23 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (11.51%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -4,7 +4,7 @@ public:
         //take every node from 1 to n as root
         //then depending on root, some numbers can be only on left and some on right
         //the numbers on left and right can also be arranged in different structures
-        //that is numTrees(#leftnodes) + numTress(#rightNodes)
+        //that is numTrees(#leftnodes) + numTrees(#rightNodes)
         //so we also need these, store in dp
         //eg. for n=4, (#leftNodes,#rightNodes) -
         //(1,2),(2,1),(0,3),(3,0)
