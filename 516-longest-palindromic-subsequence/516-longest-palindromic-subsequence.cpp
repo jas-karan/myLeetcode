93758b2c1f57d3de92942c4ffeadd83838340404
Jaskaran Singh <262jaskaran@gmail.com>
2022-02-16 18:18:31 +0530

    Time: 55 ms (92.87%), Space: 10.6 MB (90.15%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -5,6 +5,11 @@ public:
         
         //or
         //results for lps of s(i..j) using GAP method
+        //one char string for example "a" is always palindrome
+        //two char string for eg. "ab"- has 4 possible sequences - "","b" ,"a","ab"
+        //if two chars are equal => longest is 2, else 1
+        
+        
         
         int n = s.length();
         int dp[n][n];
@@ -18,8 +23,10 @@ public:
                     dp[i][j]= (s[i]==s[j])?2:1; 
                 }
                 else{
-                    if(s[i]==s[j]) dp[i][j]=2+dp[i+1][j-1];
+                    if(s[i]==s[j]) dp[i][j]=2+dp[i+1][j-1]; //s[i]ands[j](2 char) added
+                    
                     else dp[i][j]=max(dp[i+1][j], dp[i][j-1]);
+                    //exclude s[i] and s[j] and take max
                 }
                 
             }
