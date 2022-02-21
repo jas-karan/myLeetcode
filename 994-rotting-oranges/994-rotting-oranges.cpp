3df14fc69e58e2caf492b2ff348b8e3625a59994
Jaskaran Singh <262jaskaran@gmail.com>
2022-02-21 19:23:55 +0530

    Time: 7 ms (77.83%), Space: 13.1 MB (81.12%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,43 +1,46 @@
 class Solution {
 public:
+    //same as zero one matrix - ie, find closest zero to each one
+    
     int dir[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
     
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size(),n = grid[0].size();
         int oranges = 0;
-        queue<pair<pair<int,int>,int>>q;
+        queue<pair<int,int>>q;
+        
+        //store rotten oranges to queue
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
                     oranges++;
-                    grid[i][j]=-1;
                 }
-                else if(grid[i][j]==2) q.push({{i,j},0});
+                else if(grid[i][j]==2) q.push({i,j});
             }
         }
         
-        int time = 0;
+        int time = 2;
         int contaminated = 0;
         while(!q.empty()){
-            int i = q.front().first.first;
-            int j = q.front().first.second;
-            int dis = q.front().second;
+            int i = q.front().first;
+            int j = q.front().second;
             q.pop();
             
             for(int d=0;d<4;d++){
                 int i_ = i+dir[d][0];
                 int j_ = j+dir[d][1];
                 
-                if(i_>=0 && j_>=0 && i_<m && j_<n && grid[i_][j_]==-1){
-                    grid[i_][j_]=dis+1;
-                    q.push({{i_,j_},dis+1});
+                if(i_>=0 && j_>=0 && i_<m && j_<n && grid[i_][j_]==1){
+                    grid[i_][j_]=grid[i][j]+1;
+                    q.push({i_,j_});
                     contaminated++;
-                    time = max(time,dis+1);
+                    time = max(time,grid[i_][j_]);
                 }
             }
         }
         
         if(contaminated!=oranges) return -1;
-        return time;
+        
+        return time-2;
     }
 };
