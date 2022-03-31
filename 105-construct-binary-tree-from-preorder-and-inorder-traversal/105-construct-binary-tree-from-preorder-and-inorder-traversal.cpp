3d3a93ad283621743e9d14c23292c9a1f1994a8c
Jaskaran Singh <262jaskaran@gmail.com>
2022-03-31 19:59:54 +0530

    Time: 12 ms (95.63%), Space: 26.5 MB (26.45%) - LeetHub

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *helper(vector<int> &preorder, vector<int> &inorder, int start, int end, int &index, unordered_map<int, int> &dict) {
+        if (start > end) return NULL;
+    
+        TreeNode *node = new TreeNode(preorder[index]);
+        int i = dict[preorder[index++]];
+        node -> left = helper(preorder, inorder, start, i - 1, index, dict);
+        node -> right = helper(preorder, inorder, i + 1, end, index, dict);
+        return node;
+    }
+    
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int index = 0;
+        unordered_map<int, int> dict;
+        
+        // Store all the inorder indices to get start and end for each node's left and right nodes
+        for (int i = 0; i < inorder.size(); ++i) 
+            dict[inorder[i]] = i;
+        
+        
+        return helper(preorder, inorder, 0, preorder.size() - 1, index, dict);
+    }
+};
