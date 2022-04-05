dc94dfd894ec1e4e8fc745700c3640d00836f6dd
Jaskaran Singh <262jaskaran@gmail.com>
2022-04-05 09:14:05 +0530

    Time: 171 ms (29.53%), Space: 67.8 MB (54.71%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -8,6 +8,11 @@ public:
             curr+=n;
             sum=max(curr,sum);
             if(curr<0) curr=0;
+            //why ? - if curr sum is negative
+            //if next number is negative then it will further decrease curr
+            //so it is better that start next subarray from next element
+            //if next is positive let curr = -2 and next element is 5
+            //then -2+5 or 5 ,obviously 5 is better so start from next element new subarray
         }
         
         return sum;
