5f714ba7a6d7bceaa2949b6b4999fa565465de5a
Jaskaran Singh <262jaskaran@gmail.com>
2022-04-05 08:33:24 +0530

    Time: 152 ms (24.36%), Space: 59.1 MB (38.72%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    int maxArea(vector<int>& h) {
+    int maxArea(vector<int>&h) {
+        //here we are finding container with max water
+        //in trapping rain water, we find total water that can be collected over bars
+        
+        
         int ans = 0;
         int i=0,j=h.size()-1;
         
         while(i<j){
-            int curr ;
-            if(h[i]<=h[j]){
-                curr = (j-i)*h[i];
-                i++;
-            }
-            else{
-                curr = (j-i)*h[j];
-                j--;
-            }
+            int ht = h[i]<=h[j] ? h[i]:h[j];
+            
+            int curr = (j-i)*ht;
+            
+            while(i<j && h[i]<=ht) i++;  //move to next greater left line
+            //why not smaller - (j-i) will decrease and ht will also decrease so that will not optimise
+            
+            while(i<j && h[j]<=ht) j--;  //move to next greatr right line
             ans = max(ans,curr);
         }
         
