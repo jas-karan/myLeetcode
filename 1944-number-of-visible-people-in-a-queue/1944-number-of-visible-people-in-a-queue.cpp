fd02a3bb4cb6433cfad7bbf94b6dc2cf588b2a5a
Jaskaran Singh <262jaskaran@gmail.com>
2022-04-26 22:44:36 +0530

    Time: 196 ms (76.80%), Space: 84 MB (73.81%) - LeetHub

---
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> canSeePersonsCount(vector<int>& A) {
+        int n = A.size();
+        
+        vector<int> res(n);
+        stack<int>st;
+        
+        for (int i = 0; i < n; ++i) {
+            
+            //shorter elements can see this element
+            while (!st.empty() && A[st.top()] <= A[i])
+                res[st.top()]++, st.pop();
+            
+            //if there is a longer element, it can view current element
+            //(in stack, elements will be in decreasing order)
+            if (!st.empty())
+                res[st.top()]++;
+            st.push(i);
+        }
+        return res;
+        
+        
+    }
+};
