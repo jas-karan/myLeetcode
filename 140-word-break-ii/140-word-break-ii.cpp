285c8591e9257afec4f81a02a3bcefb534c0cf4b
Jaskaran Singh <262jaskaran@gmail.com>
2022-05-26 13:18:46 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (63.82%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.cpp
+++ 140-word-break-ii/140-word-break-ii.cpp
@@ -2,30 +2,77 @@ class Solution {
 public:
     unordered_set<string>dict;
     
-    void helper(vector<string>&res, string curr, string s){
-        if(s.length()==0){
-            curr.pop_back();  //last space removal
-            res.push_back(curr);
-            return;
-        }
-        string temp = "";
-        for(int i=0;i<s.length();i++){
-            temp+=s[i];
-            if(dict.find(temp) != dict.end()){
-                helper(res,curr+temp+" ",s.substr(i+1));
+//     void helper(vector<string>&res, string curr, string s){
+//         if(s.length()==0){
+//             curr.pop_back();  //last space removal
+//             res.push_back(curr);
+//             return;
+//         }
+//         string temp = "";
+//         for(int i=0;i<s.length();i++){
+//             temp+=s[i];
+//             if(dict.find(temp) != dict.end()){
+//                 helper(res,curr+temp+" ",s.substr(i+1));
+//             }
+//         }
+        
+//     }
+    
+    //word break 1
+    bool canBreak(string s){
+        int n = s.length();
+        
+        vector<bool>dp(n+1,false);
+        
+        dp[0] = true; //an empty string can be made
+        
+        for(int i=1;i<=n;i++){
+            
+            for(int j=i-1;j>=0;j--){
+                
+                string temp = s.substr(j,i-j);  
+                
+                if(dict.find(temp)!=dict.end())  //if from jth index is in dict
+                    dp[i] = dp[i]||dp[j];   //and till j-1 th index is breakable
+                
+                if(dp[i]) break;  //till i-1 th index is breakable
             }
         }
         
+        return dp[n];
     }
     
+    //word break 2
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        vector<string>res;
-        string curr="";
+        // vector<string>res;
+        // string curr="";
         
         for(string w:wordDict) dict.insert(w);
         
-        helper(res,curr,s);
+        // helper(res,curr,s);
         
-        return res;
+        if(!canBreak(s)) return {}; 
+        
+        int n = s.length();
+        vector<vector<string>>dp(n+1);
+        
+        dp[0].emplace_back("");
+        
+        for(int i=1;i<=n;i++){
+            
+            for(int j=i-1;j>=0;j--){
+                
+                string temp = s.substr(j,i-j);
+                
+                if(dict.find(temp)!=dict.end()){
+                    for(string t:dp[j]){
+                        if(j==0) dp[i].push_back(temp);
+                        else dp[i].push_back(t+" "+temp);
+                    }
+                }
+            }
+        }
+        
+        return dp[n];
     }
 };
