857e22604760b3853871a324de36ec35f82df793
Jaskaran Singh <262jaskaran@gmail.com>
2022-05-27 10:08:58 +0530

    Time: 24 ms (28.52%), Space: 17.8 MB (36.89%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.cpp
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -6,18 +6,23 @@ public:
         int n = a.size();
         
         for(int i=0;i<n;i++){
-            
-            while(!st.empty() && (st.top()>0 && a[i]<0) && abs(st.top())<abs(a[i])){
-                st.pop();
-            }
-            
-            if(!st.empty() && (st.top()>0 && a[i]<0) && abs(st.top())==abs(a[i])){
-                st.pop();
-            }
-            
-            else if(st.empty() || !(st.top()>0 && a[i]<0)){
+            //add positives to stack
+            if(a[i]>0 || st.empty()){
                 st.push(a[i]);
+                continue;
             }
+            
+            //a[i] is negative and will destroy all positive st.tops
+            while(!st.empty() && st.top()>0 && st.top()<abs(a[i])){
+                st.pop();
+            }
+            
+            if(!st.empty() && st.top()>0 && st.top()==abs(a[i])){
+                st.pop();
+            }
+            
+            else if(st.empty() || st.top()<0)
+                st.push(a[i]);
             
         }
         
