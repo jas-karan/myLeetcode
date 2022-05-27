f7de3ebec88f01a4b0358df7d249c74565d8e015
Jaskaran Singh <262jaskaran@gmail.com>
2022-05-27 12:33:31 +0530

    Time: 246 ms (67.57%), Space: 26.2 MB (79.73%) - LeetHub

---
+++ 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int dp[2001][2001] = {} , mx_val = 2001;
+    
+    int solve(vector<int>&a,vector<int>&b,int i,int prev)
+    {
+        if(i >= a.size())
+            return 0;
+         
+        // take the index of element in array b which is greater than prev
+        int j = upper_bound(b.begin() , b.end() , prev) - b.begin();
+        
+        // if prev is greater than the last element of a then we are unable to sort the array a
+        if( prev >= a[i] && j >= b.size() )
+            return mx_val;
+        
+        if(dp[i][j] == 0) dp[i][j] = j < b.size() ? 1 + solve(a,b,i+1,b[j]):mx_val ;
+        // if prev elemnt of array a is smaller than next than no need of conversion 
+        
+        if(prev < a[i])
+            dp[i][j] = min(dp[i][j] , solve(a,b,i+1,a[i]));
+        
+        return dp[i][j];
+    }
+    int makeArrayIncreasing(vector<int>& a, vector<int>& b) {
+        
+        // sort the second aray in order to start with the smallest element
+        sort(b.begin() , b.end());
+        int p = solve(a,b,0,-1e9);
+        return p >= mx_val ? -1 : p;
+    }
+};
