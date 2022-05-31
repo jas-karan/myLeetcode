d471915bc8860e36fa6c07caa61c1790422c380b
Jaskaran Singh <262jaskaran@gmail.com>
2022-05-31 13:25:49 +0530

    Time: 143 ms (76.40%), Space: 194.6 MB (21.29%) - LeetHub

---
+++ 403-frog-jump/403-frog-jump.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    unordered_map<int,int>ind;
+    
+    bool dfs(int lastjump, int curr, vector<vector<int>>&dp, int reqd){
+        if(curr==reqd) return true;
+        
+        if(dp[ind[curr]][lastjump]!=-1) return dp[ind[curr]][lastjump];
+        
+        bool t=false;
+        for(int i=lastjump-1;i<=lastjump+1;i++){
+            if(curr+i!=curr && ind.find(curr+i)!=ind.end()){
+                t = t|| dfs(i,curr+i,dp,reqd);
+            }
+        }
+        
+        return dp[ind[curr]][lastjump]=t;
+    }
+    
+    bool canCross(vector<int>& s) {
+        int n=s.size();
+        if(n>=2 && s[1]!=1) return false;
+        if(n<=2) return true;
+        
+        for(int i=0;i<n;i++) ind[s[i]]=i;
+        
+        vector<vector<int>> dp(n,vector<int>(1000,-1));
+        return dfs(1,1,dp,s[n-1]);
+    }
+};
