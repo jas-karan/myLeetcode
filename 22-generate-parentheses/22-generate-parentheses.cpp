8ff98b73a55e2eaa63df68d134ab8ad6219c796b
Jaskaran Singh <262jaskaran@gmail.com>
2022-05-31 17:05:10 +0530

    Time: 9 ms (30.72%), Space: 13.8 MB (37.84%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    vector<string> generateParenthesis(int n) {
-        vector<string> res;
-        addingpar(res, "", n, 0);
-        return res;
-    }
-    void addingpar(vector<string> &v, string str, int n, int m){
-        if(n==0 && m==0) {
-            v.push_back(str);
+    vector<string>res;
+    
+    void dfs(int o,int c,string curr){
+        if(o==0 && c==0){
+            res.push_back(curr);
             return;
         }
-        if(m > 0){ addingpar(v, str+")", n, m-1); }
-        if(n > 0){ addingpar(v, str+"(", n-1, m+1); }
+        
+        if(o!=0) dfs(o-1,c+1,curr+"(");
+        if(c!=0) dfs(o,c-1,curr+")");
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        dfs(n,0,"");
+        return res;
     }
 };
