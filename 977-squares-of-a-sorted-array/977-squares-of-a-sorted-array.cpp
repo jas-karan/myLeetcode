1245e6cfb39322c9f5baaf8e26b9a49d3e594e7c
Jaskaran Singh <262jaskaran@gmail.com>
2022-06-02 08:17:23 +0530

    Time: 61 ms (21.59%), Space: 25.9 MB (55.69%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& nums) {
-        vector<int>res;
-        int sz = nums.size();
-        int p=sz,n=-1;
+    vector<int> sortedSquares(vector<int>& a) {
+//         vector<int>res;
+//         int sz = nums.size();
+//         int p=sz,n=-1;
         
-        for(int i=0;i<sz;i++){
-            if(nums[i]<0) n=i;
-        }
-        p=n+1;
+//         for(int i=0;i<sz;i++){
+//             if(nums[i]<0) n=i;
+//         }
+//         p=n+1;
         
-        while(n>=0 && p<sz){
-            if(abs(nums[p])<=abs(nums[n])){
-                res.push_back(nums[p]*nums[p]);
-                p++;
-            }
-            else{
-                res.push_back(nums[n]*nums[n]);
-                n--;
-            }
-        }
+//         while(n>=0 && p<sz){
+//             if(abs(nums[p])<=abs(nums[n])){
+//                 res.push_back(nums[p]*nums[p]);
+//                 p++;
+//             }
+//             else{
+//                 res.push_back(nums[n]*nums[n]);
+//                 n--;
+//             }
+//         }
         
-        while(n>=0){
-            res.push_back(nums[n]*nums[n]);
-            n--;
-        }
-        while(p<sz){
-            res.push_back(nums[p]*nums[p]);
-            p++;
-        }
+//         while(n>=0){
+//             res.push_back(nums[n]*nums[n]);
+//             n--;
+//         }
+//         while(p<sz){
+//             res.push_back(nums[p]*nums[p]);
+//             p++;
+//         }
         
-        return res;
+//         return res;
+        
+        int i = 0, j = a.size() - 1, k = j;
+        vector <int> ret(k + 1, 0);
+        while (k >= 0) {
+            if (abs(a[i]) > abs(a[j])) ret[k--] = a[i] * a[i++];
+            else ret[k--] = a[j] * a[j--];
+        }
+        return ret;
     }
 };
