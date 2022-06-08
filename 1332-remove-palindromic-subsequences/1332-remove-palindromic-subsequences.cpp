45bf94c70d3c6799e308cc5ae5b14a74b7cc7c05
Jaskaran Singh <262jaskaran@gmail.com>
2022-06-08 21:22:44 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (78.83%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    int removePalindromeSub(string s) {
-        string temp = s;
-        reverse(s.begin(), s.end());
-        if(s == temp) return 1;
-        return 2;
+    int removePalindromeSub(string S) {
+        if (S == "") return 0;
+        for (int i = 0, j = S.size() - 1; i < j; i++, j--)
+            if (S[i] != S[j]) return 2;
+        return 1;
     }
 };
