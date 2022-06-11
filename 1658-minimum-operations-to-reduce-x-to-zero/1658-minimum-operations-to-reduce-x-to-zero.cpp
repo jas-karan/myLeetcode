78df1a3fb088a64818c302ddf2ab50beaf99dcd8
Jaskaran Singh <262jaskaran@gmail.com>
2022-06-11 17:11:15 +0530

    Time: 332 ms (48.70%), Space: 98.5 MB (71.00%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -9,18 +9,19 @@ public:
         if(sum==0) return nums.size();
         if(sum<0) return -1;
         
-        unordered_map<int,int>map;
-        
+       
         int curr = 0;
         int ans=-1;
-        for(int i=0;i<nums.size();i++){
-            curr+=nums[i];
-            if(curr==sum) ans=i+1;
-            else if(map.find(curr-sum)!=map.end()){
-                ans = max(ans,i-map[curr-sum]);
+        int l=0;
+        for(int r=0;r<nums.size();r++){
+            curr+=nums[r];
+            while(curr>sum){
+                curr-=nums[l];
+                l++;
             }
-            
-            map[curr]=i;
+            if(curr==sum){
+                ans=max(ans,r-l+1);
+            }
         }
         
         if(ans==-1) return -1;
