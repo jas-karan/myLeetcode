5f1211d8bc8c8beb121eec2ccc7910e02f94032d
Jaskaran Singh <262jaskaran@gmail.com>
2022-06-16 08:45:34 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (99.69%) - LeetHub

---
+++ 949-largest-time-for-given-digits/949-largest-time-for-given-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string largestTimeFromDigits(vector<int>& A) {
+        
+        sort(A.begin(), A.end());
+        
+        string res = "";
+        
+        do {
+            string hours = {(char)(A[0] + '0'), (char)(A[1] + '0')};
+            string minutes = {(char)(A[2] + '0'), (char)(A[3] + '0')};
+            
+            res = (stoi(hours) <= 23 && stoi(minutes) <= 59) ? (hours + ":" + minutes) : res;
+            
+        } while(next_permutation(A.begin(), A.end()));
+        return res;
+    }
+};
