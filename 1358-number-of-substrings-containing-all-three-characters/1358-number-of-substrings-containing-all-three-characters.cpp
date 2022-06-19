f90a1d755095929adace875dd7bf123786fb977e
Jaskaran Singh <262jaskaran@gmail.com>
2022-06-19 18:08:57 +0530

    Time: 116 ms (22.00%), Space: 8.5 MB (49.75%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -18,19 +18,32 @@ public:
 //             }
 //         }
         
-        vector<int>a(n,n),b(n,n),c(n,n);
-        int ai = n,bi = n,ci = n;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]=='a') ai=i;
-            else if(s[i]=='b') bi=i;
-            else ci=i;
+//         vector<int>a(n,n),b(n,n),c(n,n);
+//         int ai = n,bi = n,ci = n;
+//         for(int i=n-1;i>=0;i--){
+//             if(s[i]=='a') ai=i;
+//             else if(s[i]=='b') bi=i;
+//             else ci=i;
             
-            a[i]=ai; b[i]=bi; c[i]=ci;
-        }
+//             a[i]=ai; b[i]=bi; c[i]=ci;
+//         }
         
         
-        for(int i=0;i<n;i++){
-            res+=n-max({a[i],b[i],c[i]});
+//         for(int i=0;i<n;i++){
+//             res+=n-max({a[i],b[i],c[i]});
+//         }
+//         return res;
+        
+        map<char, int> f;
+        int l=0, r=0;
+        
+        while(r < n) {
+            f[s[r]]++;
+            while(f['a'] && f['b'] && f['c']) {
+                res += n-r;
+                f[s[l++]]--;
+            }
+            r++;
         }
         return res;
     }
