ccfaeace24daf4757cc48fdaab3a573aa3b4d8d3
Jaskaran Singh <262jaskaran@gmail.com>
2022-06-19 16:25:45 +0530

    Time: 283 ms (99.60%), Space: 0B (100.00%) - LeetHub

--- 197-rising-temperature/197-rising-temperature.sql
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -1,8 +1,14 @@
 # Write your MySQL query statement below
 
-select w.id 
-from weather w join weather w1
-on datediff(w.recordDate,w1.recordDate)=1
-and w.temperature>w1.temperature;
+# select w.id 
+# from weather w join weather w1
+# on datediff(w.recordDate,w1.recordDate)=1
+# and w.temperature>w1.temperature;
 
 #inner join->only those records will be joined for which join cond true
+
+SELECT Weather.Id 
+FROM   Weather 
+       JOIN Weather AS w 
+         ON w.RecordDate = SUBDATE(Weather.RecordDate, 1) 
+WHERE  Weather.Temperature > w.Temperature 
