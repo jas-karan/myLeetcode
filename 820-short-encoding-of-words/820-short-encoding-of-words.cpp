cecc172e9bd88916447e6e6b52461a946a889b9a
Jaskaran Singh <262jaskaran@gmail.com>
2022-06-20 10:26:21 +0530

    Time: 47 ms (95.54%), Space: 17.3 MB (81.19%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int minimumLengthEncoding(vector<string>& words) {
         unordered_set<string> s(words.begin(), words.end());
+        //remove all the suffices bcoz those can be included in curr string
         for (string w : s)
             for (int i = 1; i < w.size(); ++i)
                 s.erase(w.substr(i));
