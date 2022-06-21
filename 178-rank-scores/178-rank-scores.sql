8e4fe848e0dd57c91b8b305bc23c475eeb5f3113
Jaskaran Singh <262jaskaran@gmail.com>
2022-06-21 16:50:22 +0530

    Time: 790 ms (9.95%), Space: 0B (100.00%) - LeetHub

---
+++ 178-rank-scores/178-rank-scores.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+SELECT Scores.Score, COUNT(Ranking.Score) AS `RANK`
+  FROM Scores
+     , (
+       SELECT DISTINCT Score
+         FROM Scores
+       ) Ranking
+ WHERE Scores.Score <= Ranking.Score
+ GROUP BY Scores.Id, Scores.Score
+ ORDER BY Scores.Score DESC;
