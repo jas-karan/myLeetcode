a4b3b1deb6b84f68c1bca847aff46def43156991
Jaskaran Singh <262jaskaran@gmail.com>
2022-06-21 11:11:27 +0530

    Time: 3 ms (71.04%), Space: 8.4 MB (27.69%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
@@ -1,19 +1,66 @@
 class Solution {
 public:
+    int n = 3;
     string tictactoe(vector<vector<int>>& moves) {
-        vector<int> A(8,0), B(8,0); // 3 rows, 3 cols, 2 diagonals
-        for(int i=0; i<moves.size(); i++) {
-          int r=moves[i][0], c=moves[i][1];
-          vector<int>& player = (i%2==0)?A:B;
-          player[r]++;
-          player[c+3]++; 
-          if(r==c) player[6]++;
-          if(r==2-c) player[7]++;
-      }
-        for(int i=0; i<8; i++) {
-          if(A[i]==3) return "A";
-          if(B[i]==3) return "B";
-      }
-        return moves.size()==9 ? "Draw":"Pending";
+        vector<vector<char>>board(n,vector<char>(n,'$'));
+        for(int i = 0; i < moves.size(); i++){
+            int row = moves[i][0];
+            int col = moves[i][1];
+            if((i & 1) == 0){
+                //even, X's move
+                board[row][col] = 'X';
+                if(didWin(board, row, col, 'X')) return "A";
+            }else{
+                //odd, O's move
+                board[row][col] = 'O';
+                if(didWin(board, row, col, 'O')) return "B";
+            }
+        }
+        return moves.size() == n * n ? "Draw" : "Pending";
+    }
+    
+    bool didWin(vector<vector<char>>&board, int row, int col, char player){
+        //check the current row
+        bool didPlayerWin = true;
+        for(int i = 0; i < n; i++){
+            if(board[row][i] != player){
+                didPlayerWin = false;
+            }                
+        }
+        if(didPlayerWin) return true;   //player has won the game
+        
+        //check the current col
+        didPlayerWin = true;
+        for(int i = 0; i < n; i++){
+            if(board[i][col] != player){
+                didPlayerWin = false;
+            }                
+        }
+        if(didPlayerWin) return true;   //player has won the game
+        
+        //check the diagonal
+        if(row == col){
+            didPlayerWin = true;
+            for(int i = 0; i < n; i++){
+                if(board[i][i] != player){
+                    didPlayerWin = false;
+                }                
+            }
+            if(didPlayerWin) return true;   //player has won the game    
+        }
+        
+        //check reverse diagonal
+        if(col == n - row - 1){
+            didPlayerWin = true;
+            for(int i = 0; i < n; i++){
+                if(board[i][n-i-1] != player){
+                    didPlayerWin = false;
+                }                
+            }
+            if(didPlayerWin) return true;   //player has won the game    
+        }
+        
+        //player did not win
+        return false;
     }
 };
