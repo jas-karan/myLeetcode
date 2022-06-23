e77b7d1cc1c2311f989c126bb99006ce44e3f934
Jaskaran Singh <262jaskaran@gmail.com>
2022-06-23 08:14:16 +0530

    Time: 369 ms (47.70%), Space: 63.4 MB (29.84%) - LeetHub

---
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+     static bool cmp(vector<int>&a, vector<int>&b) {
+        return a[1]<b[1];
+    }
+    int scheduleCourse(vector<vector<int>>& courses) {
+        sort(courses.begin(),courses.end(),cmp);
+        priority_queue<int>pq;
+        int time_taken= 0;
+        for(auto c : courses) {
+            if(time_taken+c[0]<=c[1]){
+                time_taken+=c[0]; 
+                pq.push(c[0]);
+            }
+            else {
+                if(!pq.empty() && pq.top()>c[0]){
+                    int top = pq.top();
+                    pq.pop();
+                    time_taken-=top;
+                    pq.push(c[0]);
+                    time_taken+=c[0];
+                }
+            }
+        }
+        return pq.size();
+    }
+
+};
