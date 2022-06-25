313093147bd5938b20f1a700302612ab00d070b9
Jaskaran Singh <262jaskaran@gmail.com>
2022-06-25 09:02:07 +0530

    Time: 444 ms (69.16%), Space: 113.3 MB (31.72%) - LeetHub

---
+++ 1713-minimum-operations-to-make-a-subsequence/1713-minimum-operations-to-make-a-subsequence.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minOperations(vector<int>& target, vector<int>& arr) {
+        //longest common subsequence = TLE
+        //LCS to LIS (by indices)
+        //[6,4,8,1,3,2] -> [0,1,2,3,4,5]
+        //[4,7,6,2,3,8,6,1] -> [1,-1,0,5,4,2,0,3]  
+        
+        unordered_map<int, int> m;
+        
+        int n = target.size();
+        
+        for (int i = 0; i < n; ++i) m[target[i]] = i;
+        
+        vector<int> indexes;
+        for (auto x: arr) {
+            if (m.find(x) != m.end()) indexes.push_back(m[x]);
+        }
+        
+        vector<int> lis;
+        for (auto x : indexes) {
+            auto it = lower_bound(lis.begin(), lis.end(), x);
+            if (it == lis.end()) lis.push_back(x); 
+            else *it = x; 
+        }
+        return n - lis.size();
+    }
+};
