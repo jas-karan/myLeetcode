9a99698cafab6a5c0c7ee15e1489a7be5bb58646
Jaskaran Singh <262jaskaran@gmail.com>
2022-06-29 22:52:43 +0530

    Time: 565 ms (9.00%), Space: 183.3 MB (9.00%) - LeetHub

---
+++ 1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int rearrangeSticks(int n, int k) {
+        //out of k sticks, while putting ith stick 
+        //assume that larger sticks than i are already arranged
+        //putting ith stick such that j sticks are visible
+        //dp[i][j] = dp[i-1][j-1] + dp[i-1][j]*(i-1)
+        //put ith stick to front ->it will be visible
+        //put ith stick somewhere in (i-1) positions
+        
+        vector<vector<long>>dp(n+1,vector<long>(n+1,0));
+        
+        dp[1][1]=1;
+        
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                dp[i][j] = dp[i-1][j-1];
+                dp[i][j] += (dp[i-1][j])*(i-1);
+                dp[i][j]%=1000000007;
+            }
+        }
+        
+        return dp[n][k];
+    }
+};
