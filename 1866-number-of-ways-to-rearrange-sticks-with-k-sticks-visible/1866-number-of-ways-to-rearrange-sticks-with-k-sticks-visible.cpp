728040ad7fd383544833a0392af9a5e5081a1463
Jaskaran Singh <262jaskaran@gmail.com>
2022-06-29 22:54:04 +0530

    Time: 554 ms (9.00%), Space: 119.8 MB (17.50%) - LeetHub

--- 1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible.cpp
+++ 1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible.cpp
@@ -8,12 +8,12 @@ public:
         //put ith stick to front ->it will be visible
         //put ith stick somewhere in (i-1) positions
         
-        vector<vector<long>>dp(n+1,vector<long>(n+1,0));
+        vector<vector<long>>dp(n+1,vector<long>(k+1,0));
         
         dp[1][1]=1;
         
         for(int i=2;i<=n;i++){
-            for(int j=1;j<=i;j++){
+            for(int j=1;j<=min(i,k);j++){
                 dp[i][j] = dp[i-1][j-1];
                 dp[i][j] += (dp[i-1][j])*(i-1);
                 dp[i][j]%=1000000007;
