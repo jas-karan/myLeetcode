729bcd51c1edf094a6035014a937c67040855b35
Jaskaran Singh <262jaskaran@gmail.com>
2022-06-30 19:49:30 +0530

    Time: 13 ms (24.62%), Space: 9.2 MB (44.67%) - LeetHub

--- 920-number-of-music-playlists/920-number-of-music-playlists.cpp
+++ 920-number-of-music-playlists/920-number-of-music-playlists.cpp
@@ -8,7 +8,7 @@ public:
         for(int i=1;i<=goal;i++){
             for(int j=1;j<=n;j++){
                 dp[i][j] = dp[i-1][j-1]*j;
-                dp[i][j] += dp[i-1][j] * max(j-k,0);
+                if(j>k) dp[i][j] += dp[i-1][j] * (j-k);
                 dp[i][j] %= 1000000007;
             }
         }
