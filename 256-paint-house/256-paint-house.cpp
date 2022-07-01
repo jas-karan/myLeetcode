12ef97ff70d151aac2762175e1151f6429f3e2d2
Jaskaran Singh <262jaskaran@gmail.com>
2022-07-01 09:02:01 +0530

    Time: 17 ms (27.67%), Space: 9.6 MB (59.75%) - LeetHub

---
+++ 256-paint-house/256-paint-house.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minCost(vector<vector<int>>& costs) {
+        for(int i=1;i<costs.size();i++){
+            for(int j=0;j<3;j++){
+                int curr=INT_MAX;
+                
+                for(int k=0;k<3;k++){
+                    if(k!=j){
+                        curr=min(curr,costs[i][j]+costs[i-1][k]);
+                    }
+                }
+                
+                costs[i][j]=curr;
+            }
+        }
+        
+        int cost=INT_MAX;
+        for(int j=0;j<3;j++) cost=min(cost,costs[costs.size()-1][j]);
+        
+        return cost;
+    }
+};
