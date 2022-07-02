00f8a1b18a3c8f18e60d6c886a12ec6630fc35cf
Jaskaran Singh <262jaskaran@gmail.com>
2022-07-02 16:26:19 +0530

    Time: 176 ms (7.07%), Space: 197.7 MB (12.70%) - LeetHub

---
+++ 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int nextlevel(string &s){
+        if(s.length()==0) return -1;
+        int i=0;
+        while(i<s.length() && s[i]=='-') i++;
+        return i;
+    }
+    
+    int val(string&s){
+        int pos=0;
+        int v=0;
+        while(pos<s.length() && s[pos]>='0' && s[pos]<='9'){ v=v*10+(s[pos]-'0'); pos++;}
+        s = s.substr(pos);
+        return v;
+    }
+    
+    TreeNode* dfs(string&s,int level){
+        int v = val(s);
+        TreeNode* root = new TreeNode(v);
+        int next = nextlevel(s);
+        if(next==level+1){
+            s=s.substr(next);
+            root->left=dfs(s,next);
+        }
+        else root->left=NULL;
+        next=nextlevel(s);
+        if(next==level+1){
+            s=s.substr(next);
+            root->right=dfs(s,next);
+        }
+        else root->right=NULL;
+        
+        return root;   
+    }
+    
+    TreeNode* recoverFromPreorder(string traversal) {
+        return dfs(traversal,0);
+    }
+};
