b78c442e5aa9f9527f86c0d9d2c42d8a75d9cfa7
Jaskaran Singh <262jaskaran@gmail.com>
2022-07-05 18:33:27 +0530

    Time: 630 ms (34.87%), Space: 135.2 MB (35.88%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -25,43 +25,30 @@ public:
         
 //         return dfs(txnleft,prices,0,memo);
         
-        int dp[prices.size() + 1][5];
+        int n=prices.size();
+        int k=2;
+        int txns=k*2;
         
+        vector<vector<int>>dp(n+1,vector<int>(txns+1));
         
-        for(int day = (int) prices.size();day >= 0;day--){
-            
-            for(int transactionsLeft = 0;transactionsLeft <= 4;transactionsLeft++){
+        for(int i=n;i>=0;i--){
+            for(int j=0;j<=txns;j++){
                 
-                int &ans = dp[day][transactionsLeft];
-                
-                if(day == prices.size()){
-                    ans = 0;
-                }else if(transactionsLeft == 0){
-                    ans = 0;
-                }else{
+                if(i==n) dp[i][j]=0;
+                else if(j==0) dp[i][j]=0;
+                else{
+                    int ans1=dp[i+1][j];  //no txn 
+                    int ans2;
+                    bool buy_day = j%2==0 ? true:false;
+                    if(buy_day) ans2=-prices[i]+dp[i+1][j-1];
+                    else ans2=prices[i]+dp[i+1][j-1];
                     
-                    // choice 1
-                    // no transaction today
-                    int ans1 = dp[day + 1][transactionsLeft];
-                    
-                    // choice 2
-                    // doing the possible transaction today     
-                    int ans2 = 0;
-                    bool buy = (transactionsLeft % 2 == 0);
-
-                    if(buy == true){ // buy
-                        ans2 = -prices[day] + dp[day + 1][transactionsLeft - 1];
-                    }else{ // sell
-                        ans2 = prices[day] + dp[day + 1][transactionsLeft - 1];
-                    }
-
-                    ans = max(ans1, ans2);
-
+                    dp[i][j]=max(ans1,ans2);
                 }
-                
             }
         }
-        return dp[0][4];
+        
+        return dp[0][txns];
     
     }
 };
