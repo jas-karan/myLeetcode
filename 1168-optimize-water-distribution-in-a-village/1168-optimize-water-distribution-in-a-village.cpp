dfe9a581766bc8b67c0510bcb08db7925dfd6968
Jaskaran Singh <262jaskaran@gmail.com>
2022-07-06 15:40:56 +0530

    Time: 282 ms (38.45%), Space: 42.1 MB (56.84%) - LeetHub

---
+++ 1168-optimize-water-distribution-in-a-village/1168-optimize-water-distribution-in-a-village.cpp
@@ -0,0 +1,52 @@
+class UnionFind {
+private: vector<int>root;
+        
+public:
+    int cnt;
+    UnionFind(int size) {
+        cnt=size;
+        root = vector<int>(size);
+        for (int i = 0; i < size; i++) {
+            root[i] = i;
+        }
+    }
+
+    int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        return root[x] = find(root[x]);
+    }
+
+    bool union_(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if( rootX == rootY) return false;
+        root[rootY] = rootX;
+        cnt--;
+        return true;
+    }
+
+    bool connected(int x, int y) {
+        return find(x) == find(y);
+    }
+};
+
+class Solution {
+public:
+    int minCostToSupplyWater(int n, vector<int>& wells, vector<vector<int>>& pipes) {
+        UnionFind uf(n+1);
+        for(int i=1;i<=n;i++){
+            pipes.push_back({0,i,wells[i-1]});
+        }
+        sort(pipes.begin(),pipes.end(),[](vector<int>&a,vector<int>&b){
+            return a[2]<b[2];
+        });
+        
+        int cost=0;
+        for(auto p:pipes){
+            if(uf.union_(p[0],p[1])) cost+=p[2];
+        }
+        return cost;
+    }
+};
