2ddab3d2c42878865d948100ba546205e34176f8
Jaskaran Singh <262jaskaran@gmail.com>
2022-07-06 10:52:56 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.05%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int fib(int N) {
+        if (N <= 1) {
+            return N;
+        }
+
+        int current = 0;
+        int prev1 = 1;
+        int prev2 = 0;
+
+        for (int i = 2; i <= N; i++) {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return current;
+        
+    }
+};
