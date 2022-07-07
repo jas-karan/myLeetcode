02f7c5881d8b4f9215ac0096db3afa822c475911
Jaskaran Singh <262jaskaran@gmail.com>
2022-07-07 21:29:30 +0530

    Time: 30 ms, Memory: 17 MB - LeetHub

--- all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>>res;
-    int n;
-    bool vis[16]={false};
-    
-    void dfs(vector<vector<int>>& graph,int node,vector<int>curr){
-        if(node==n-1){
-            curr.push_back(n-1);
-            res.push_back(curr);
-            return;
-        }
-        
-        vis[node]=true;
-        curr.push_back(node);
-        
-        for(int nbr:graph[node]){
-            if(vis[nbr]==false) dfs(graph,nbr,curr);
-        }
-        
-        vis[node]=false;
-        curr.pop_back() ; 
-    }
-    
+   
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int n=graph.size();
         
-        vector<int>curr;
-        n=graph.size();
-        dfs(graph,0,curr);
+        queue<pair<int,vector<int>>>q;
+        q.push({0,{0}});
+        vector<vector<int>>res;
+        
+        while(!q.empty()){
+            auto cur = q.front(); q.pop();
+            
+            if(cur.first==n-1){
+                res.push_back(cur.second);
+                continue;
+            }
+            
+            for(int nbr:graph[cur.first]){
+                cur.second.push_back(nbr);
+                q.push({nbr,cur.second});
+                cur.second.pop_back();
+            }
+        }
+        
+        
         return res;
     }
 };
