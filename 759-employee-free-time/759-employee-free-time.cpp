f048a6dfedeb548cd5308ba36e0f4d518eb49534
Jaskaran Singh <262jaskaran@gmail.com>
2022-07-11 18:02:34 +0530

    Time: 28 ms (98.73%), Space: 11.2 MB (50.21%) - LeetHub

---
+++ 759-employee-free-time/759-employee-free-time.cpp
@@ -0,0 +1,40 @@
+/*
+// Definition for an Interval.
+class Interval {
+public:
+    int start;
+    int end;
+
+    Interval() {}
+
+    Interval(int _start, int _end) {
+        start = _start;
+        end = _end;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<Interval> employeeFreeTime(vector<vector<Interval>> schedule) {
+        vector<Interval>ans;
+        map<int,int>map;
+        
+        for(auto e:schedule){
+            for(auto i:e){
+                map[i.start]=max(map[i.start],i.end);
+            }
+        }
+        
+        int pre = map.begin()->first;
+        
+        for(auto [s,e]:map){
+            if(pre<s){
+                ans.push_back(Interval(pre,s));
+            }
+            pre = max(pre,e);
+        }
+        
+        return ans;
+    }
+};
