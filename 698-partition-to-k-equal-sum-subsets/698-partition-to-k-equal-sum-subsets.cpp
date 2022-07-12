a30d98cce33e46066c8e88e68f4c7912286c887a
Jaskaran Singh <262jaskaran@gmail.com>
2022-07-12 08:32:36 +0530

    Time: 208 ms (49.69%), Space: 34.8 MB (11.65%) - LeetHub

---
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int n;
+    unordered_map<int,bool>memo;
+    
+    bool dfs(vector<int>&nums,int ind,int k,int curr,int tar,int mask){
+        if(k==1) return true;
+        
+        if(curr>tar) return false;
+        
+        if(memo.count(mask)) return memo[mask];
+        
+        if(curr==tar){
+            return memo[mask]=dfs(nums,0,k-1,0,tar,mask);
+        }
+        
+        for(int j=ind;j<n;j++){
+            if((mask&(1<<j))==0){
+                mask = mask|(1<<j);
+                
+                if(dfs(nums,j+1,k,curr+nums[j],tar,mask)) return memo[mask]=true;
+                
+                mask = mask ^ (1<<j);
+            }
+        }
+        return memo[mask]=false;
+    }
+    
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int sum=0;
+        n=nums.size();
+        
+        for(int i:nums) sum+=i;
+        
+        if(n<k||sum%k!=0) return false;
+        
+        return dfs(nums,0,k,0,sum/k,0);
+    }
+};
