1f01142cc1bf970d122cf9bbcc9d36b22f42ffc8
Jaskaran Singh <262jaskaran@gmail.com>
2022-07-15 18:24:55 +0530

    Time: 34 ms (26.89%), Space: 13 MB (64.24%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        int maxn=0;
-        unordered_map<int,int>cnt;
+      
+        map<int,int>cnt;
         for(int i:nums){
             cnt[i]+=i;
-            maxn=max(maxn,i);
         }
         
-        vector<int>dp(maxn+1,0);
+        nums.clear();
+        for(auto e:cnt) nums.push_back(e.first); 
+        int n = nums.size();
+        vector<int>dp(n+1,0);
         dp[0]=0;
-        dp[1]=cnt[1];
+        dp[1]=cnt[nums[0]];
         
-        for(int i=2;i<=maxn;i++){
-            dp[i]=max(dp[i-1],cnt[i]+dp[i-2]);
+        for(int i=2;i<=n;i++){
+            if(nums[i-1]==nums[i-2]+1){
+                dp[i]=max(dp[i-1],cnt[nums[i-1]]+dp[i-2]);
+            }
+            else{
+                dp[i]=dp[i-1]+cnt[nums[i-1]];
+            }
         }
         
-        return dp[maxn];
+        return dp[n];
     }
 };
