a566ae9525c5b5eb0b46df2e65f48f63c560bd67
Jaskaran Singh <262jaskaran@gmail.com>
2022-07-15 17:44:26 +0530

    Time: 12 ms, Memory: 13.6 MB - LeetHub

---
+++ min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dp[1001];
+    
+    int dfs(int i,vector<int>& cost){
+        if(i>=cost.size()) return 0;
+        if(dp[i]!=-1) return dp[i];
+        return dp[i]=cost[i]+min(dfs(i+1,cost),dfs(i+2,cost));
+    }
+    
+    int minCostClimbingStairs(vector<int>& cost) {
+        // memset(dp,-1,sizeof(dp));
+        // int ans1=dfs(0,cost);
+        // int ans2=dfs(1,cost);
+        // return min(ans1,ans2);
+        
+        dp[0]=0;
+        dp[1]=0;
+        
+        for(int i=2;i<=cost.size();i++){
+            dp[i] = min(cost[i-1]+dp[i-1], cost[i-2]+dp[i-2]);
+        }
+        
+        return dp[cost.size()];
+    }
+};
