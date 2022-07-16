647e87fbfb9258d0e72937e845d5d2c69fecaea2
Jaskaran Singh <262jaskaran@gmail.com>
2022-07-16 13:09:21 +0530

    Time: 467 ms (75.00%), Space: 122.8 MB (68.68%) - LeetHub

--- 1182-shortest-distance-to-target-color/1182-shortest-distance-to-target-color.cpp
+++ 1182-shortest-distance-to-target-color/1182-shortest-distance-to-target-color.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     vector<int> shortestDistanceColor(vector<int>& colors, vector<vector<int>>& queries) {
-        vector<vector<int>> cindex(4);
-        for(int i=0;i<colors.size();i++) cindex[colors[i]].push_back(i);
-        vector<int> res;
-        for(auto& q : queries){
-            int i = q[0], c = q[1];
-            auto& v = cindex[c];
-            int j = lower_bound(v.begin(), v.end(), i) - v.begin();
-            int ans = 1e5;
-            if(j < v.size()) ans = v[j]-i;
-            if(j != 0) ans = min(ans, i - v[j-1]);
-            res.push_back(ans == 1e5 ? -1 : ans);
+        vector<vector<int>>map(4);
+        for(int i=0;i<colors.size();i++)
+            map[colors[i]].push_back(i);
+        
+        vector<int>res;
+        for(auto&q:queries){
+            int ind=q[0];
+            int c=q[1];
+            if(map[c].size()==0){
+                res.push_back(-1);
+                continue;
+            }
+            vector<int>&cur=map[c];
+            int j=lower_bound(begin(cur),end(cur),ind)-begin(cur);
+            int ans=INT_MAX;
+            
+            if(j<cur.size()) ans=cur[j]-ind;
+            if(j!=0) ans=min(ans,ind-cur[j-1]);
+            
+            res.push_back(ans);
         }
+        
         return res;
     }
 };
