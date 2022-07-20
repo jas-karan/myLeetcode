39cd86d35cb7424c9adf7eaaffa387ec08570cd6
Jaskaran Singh <262jaskaran@gmail.com>
2022-07-20 08:08:47 +0530

    Time: 326 ms (59.96%), Space: 48 MB (74.18%) - LeetHub

---
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numMatchingSubseq(string S, vector<string>& words) {
+        vector<vector<int>>fr(26);
+		for (int i = 0; i < S.size (); ++i) fr[S[i] - 'a'].push_back (i);
+		int res = 0;
+
+		for (const auto& word : words) {
+			int x = -1;
+			bool found = true;
+
+			for (char c : word) {
+				auto it = upper_bound (fr[c - 'a'].begin (), fr[c - 'a'].end (), x);
+				if (it == fr[c - 'a'].end ()) found = false;
+				else x = *it;
+                
+                if(!found) break;
+			}
+
+			if (found) res++;
+		}
+
+		return res;
+    }
+};
