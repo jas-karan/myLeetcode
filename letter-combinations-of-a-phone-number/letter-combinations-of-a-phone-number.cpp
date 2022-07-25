5525c7d0addb9e3675bacd337941e4dc028b4357
Jaskaran Singh <262jaskaran@gmail.com>
2022-07-25 13:33:55 +0530

    Time: 0 ms, Memory: 6.5 MB - LeetHub

---
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    unordered_map<int,vector<char>>mp;
+
+void call(string digits,int i,vector<string>&ans,string curr){
+	if(i==digits.length()){
+		ans.push_back(curr);
+		return;
+	}
+	int d = digits[i]-'0';
+	for(char c:mp[d]){
+		call(digits,i+1,ans,curr+c);
+	}
+}
+
+vector<string> letterCombinations(string digits) {
+    if(digits.length()==0) return {};
+	mp[2] = {'a','b','c'};
+	mp[3] = {'d','e','f'};
+	mp[4] = {'g','h','i'};
+	mp[5] = {'j','k','l'};
+	mp[6] = {'m','n','o'};
+	mp[7] = {'p','q','r','s'};
+	mp[8] = {'t','u','v'};
+	mp[9] = {'w','x','y','z'};
+	
+	vector<string>ans;
+	call(digits,0,ans,"");
+	return ans;
+}
+};
