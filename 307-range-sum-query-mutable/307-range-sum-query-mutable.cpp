e933531d00653a6f79782523f2b5a6fc5f3defec
Jaskaran Singh <262jaskaran@gmail.com>
2022-07-31 10:49:15 +0530

    Time: 748 ms (39.09%), Space: 150.7 MB (88.22%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -1,68 +1,46 @@
 class NumArray {
 public:
-    
-    int n;
-    int stree[120004];
-    
-    // Build int segmented Tree stree[0] contains total sum 
-    // stree[1] conatin sum of range 0 to mid and stree[1] conatian sum of range mid+1 to n
-    void build(int i,int s,int e,vector<int>&nums)
-    {
-        if(s == e)
-        {
-            stree[i] = nums[e];
-            return ;
-        }
-        
-        int mid = s+(e-s)/2;
-        build(2*i+1,s,mid,nums);
-        build(2*i+2,mid+1,e,nums);
-        
-        stree[i] = stree[2*i+1] + stree[2*i+2];
+    int lsb(int n){
+        return n&-n;
     }
     
-    // Updating value at particular index and updating stree accordingly
-    void treeupdate(int i ,int st, int end,int ind, int val)
-    {
-        if(st == end)
-        {
-            stree[i] = val;
-            return;
-        }
-        int mid = st+(end-st)/2;
-        if(ind<=mid)
-            treeupdate(2*i+1,st,mid,ind,val);
-        else
-            treeupdate(2*i+2,mid+1,end,ind,val);
-        
-        stree[i] = stree[i*2+1]+stree[i*2+2];
+    int sz;
+    vector<int>BIT,nums;
+    
+    void updateBIT(int ind,int val){
+        for(int i=ind;i<sz;i+=lsb(i)) BIT[i]+=val;
     }
     
-    // Return sum of range left to Right 
-    int rangeSum(int i,int st,int end,int left,int right)
-    {
-        if(left <= st && right>=end)
-            return stree[i];
-        
-        if(left>end || right<st)
-            return 0;
-        
-        int mid  = st + (end-st)/2;
-        return rangeSum(2*i+1,st,mid,left,right) + rangeSum(2*i+2,mid+1,end,left,right);
+    int sumBIT(int ind){
+        int s=0;
+        for(int i=ind;i>0;i-=lsb(i)) s+=BIT[i];
+        return s;
     }
-    
     
     NumArray(vector<int>& nums) {
-        n = nums.size();
-        build(0,0,n-1,nums);
+        sz = nums.size()+1;
+        BIT = vector<int>(sz,0);
+        this->nums = nums;
+        for(int i=1;i<sz;i++){
+            updateBIT(i,nums[i-1]);
+        }
     }
     
     void update(int index, int val) {
-        treeupdate(0,0,n-1,index,val);
+        int old = nums[index];
+        nums[index]=val;
+        updateBIT(index+1,val-old);
     }
     
     int sumRange(int left, int right) {
-        int sum = rangeSum(0,0,n-1,left,right);
-        return sum;
+        left++;right++;
+        return sumBIT(right)-sumBIT(left-1);
     }
 };
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
