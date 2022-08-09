ef7c46db741a9ba0a3b3e0bc439f6b66bb9d0fcb
Jaskaran Singh <262jaskaran@gmail.com>
2022-08-09 18:47:55 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (52.46%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -5,17 +5,15 @@ public:
         int d = 0;
         int x = 0;
         int y = 0;
-        for(int i=0;i<4;i++){
-            for(int j=0;j<s.length();j++){
-                if(s[j]=='G'){
-                    x+=dir[d][0];
-                    y+=dir[d][1];
-                }
-                else if(s[j]=='L') d=(d-1+4)%4;
-                else d = (d+1)%4;
+        for(int j=0;j<s.length();j++){
+            if(s[j]=='G'){
+                x+=dir[d][0];
+                y+=dir[d][1];
             }
-        }
+            else if(s[j]=='L') d=(d-1+4)%4;
+            else d = (d+1)%4;
+        }    
         
-        return x==0&&y==0;
+        return x==0&&y==0 || d!=0;
     }
 };
