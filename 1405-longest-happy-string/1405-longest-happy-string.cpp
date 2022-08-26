5f733c665cfe4668e56e353447f0c3374f48196a
Jaskaran Singh <262jaskaran@gmail.com>
2022-08-26 08:05:49 +0530

    Time: 5 ms (17.18%), Space: 6.2 MB (44.28%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.cpp
+++ 1405-longest-happy-string/1405-longest-happy-string.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     string longestDiverseString(int a, int b, int c) {
-        priority_queue<pair<int, char>> pq;
-        if(a != 0) pq.push({ a, 'a' });
-        if(b != 0) pq.push({ b, 'b' });
-        if(c != 0) pq.push({ c, 'c' });
-        char prev1 = '#';
-        char prev2 = '#';
-        string res;
-        while(!pq.empty())
-        {
-            auto [cnt1, ch1] = pq.top(); pq.pop();
-            if(ch1 == prev1 && ch1 == prev2)
-            {
-                if(pq.empty()) return res;
-                auto [cnt2, ch2] = pq.top(); pq.pop();
-                res += ch2;
-                prev1 = prev2;
-                prev2 = ch2;
-                pq.push({ cnt1, ch1 });
-                if(--cnt2 > 0) pq.push({ cnt2, ch2 });
+        string ans="";
+        priority_queue<pair<int,char>>pq;
+        if(a) pq.push({a,'a'});
+        if(b) pq.push({b,'b'});
+        if(c) pq.push({c,'c'});
+        char p1='$',p2='$';
+        while(pq.size()){
+            auto f = pq.top(); pq.pop();
+            if(f.second==p1&&f.second==p2){
+                if(pq.size()==0) return ans;
+                auto s = pq.top(); pq.pop();
+                ans+=s.second;
+                pq.push(f);
+                if((s.first)>1) pq.push({s.first-1,s.second});
+                p1=p2;
+                p2=s.second;
             }
-            else
-            {
-                prev1 = prev2;
-                prev2 = ch1;
-                res += ch1;
-                if(--cnt1 > 0) pq.push({ cnt1, ch1 });
+            else{
+                ans+=f.second;
+                if(f.first>1) pq.push({f.first-1,f.second});
+                p1=p2;
+                p2=f.second;
             }
         }
-        return res;
+        
+        return ans;
     }
 };
