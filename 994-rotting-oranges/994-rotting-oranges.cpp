9706d508fae19665707a40d4bc0e685d0ea147a0
Jaskaran Singh <262jaskaran@gmail.com>
2022-09-12 21:39:06 +0530

    Time: 14 ms (42.11%), Space: 12.9 MB (94.66%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,46 +1,47 @@
 class Solution {
 public:
-    //same as zero one matrix - ie, find closest zero to each one
-    
-    int dir[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+    int dir[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
     
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size(),n = grid[0].size();
-        int oranges = 0;
+        
+        int rottenOranges=0;
+        int oranges=0;
+        int m = grid.size(), n=grid[0].size();
         queue<pair<int,int>>q;
         
-        //store rotten oranges to queue
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
+                if(grid[i][j]==2){
+                    rottenOranges++;
+                    oranges++;
+                    q.push({i,j});
+                }
+                else if(grid[i][j]==1){
                     oranges++;
                 }
-                else if(grid[i][j]==2) q.push({i,j});
             }
         }
         
         int time = 2;
-        int contaminated = 0;
-        while(!q.empty()){
-            int i = q.front().first;
-            int j = q.front().second;
+        while(q.size()){
+            auto curr = q.front();
+            int i = curr.first;
+            int j = curr.second;
             q.pop();
-            
+            int t = grid[i][j];
+            time = max(time,t);
             for(int d=0;d<4;d++){
-                int i_ = i+dir[d][0];
-                int j_ = j+dir[d][1];
+                int x=i+dir[d][0];
+                int y=j+dir[d][1];
                 
-                if(i_>=0 && j_>=0 && i_<m && j_<n && grid[i_][j_]==1){
-                    grid[i_][j_]=grid[i][j]+1;
-                    q.push({i_,j_});
-                    contaminated++;
-                    time = max(time,grid[i_][j_]);
+                if(x>=0 && y>=0 && x<m && y<n && grid[x][y]==1){
+                    grid[x][y]=1+t;
+                    q.push({x,y});
+                    rottenOranges++;
                 }
             }
         }
-        
-        if(contaminated!=oranges) return -1;
-        
+        if(oranges!=rottenOranges) return -1;
         return time-2;
     }
 };
