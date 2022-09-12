b0f850dc3b07a4e004ee57bb33c1476d42a59075
Jaskaran Singh <262jaskaran@gmail.com>
2022-09-12 23:49:20 +0530

    Time: 403 ms (72.51%), Space: 8 MB (46.01%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
-    bool call(int i,int j,int m,int n,vector<vector<char>>&board,int ind,string word){
-	    if(ind==word.length()) return true;
-	
-	    if(i<0 || j<0 || i>=m || j>=n || board[i][j]!=word[ind]) return false;
-	
-	    char cw = board[i][j];
-	    board[i][j]='$';
-	    bool d = call(i+1,j,m,n,board,ind+1,word);
-	    bool u = call(i-1,j,m,n,board,ind+1,word);
-	    bool l = call(i,j+1,m,n,board,ind+1,word);
-	    bool r = call(i,j-1,m,n,board,ind+1,word);
-	
-	    board[i][j] = cw;
-	    return d||u||l||r;
+    int dir[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
+    
+    bool find(vector<vector<char>>&board,int i,int j,int m,int n,string &word,int ind){
+        if(ind==word.length()) return true;
+        
+        if(i<0 || j<0 || i>=m || j>=n || board[i][j]!=word[ind]) return false;
+        
+        char c = board[i][j];
+        board[i][j] = '$';
+        
+        bool ans = false;
+        for(int d=0;d<4;d++){
+            int x = i+dir[d][0], y = j+dir[d][1];
+            ans = ans||find(board,x,y,m,n,word,ind+1);
+        }
+        
+        board[i][j]=c;
+        return ans;
     }
     
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size(),n = board[0].size();
-	    for(int i=0;i<m;i++){
-		    for(int j=0;j<n;j++){
-			    if(board[i][j]==word[0]){
-			    	if(call(i,j,m,n,board,0,word))
-		    			return true;
-		    	}
-		    }
-	    }
-	
-	    return false;
+        int m = board.size();
+        int n = board[0].size();
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==word[0]){
+                    if(find(board,i,j,m,n,word,0)) return true;
+                }
+            }
+        }
+        
+        return false;
     }
 };
