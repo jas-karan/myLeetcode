55ed1e0cb9d10f083f0061e4f97fe5af970af5d0
Jaskaran Singh <262jaskaran@gmail.com>
2022-09-12 22:20:31 +0530

    Time: 50 ms (20.99%), Space: 14 MB (54.44%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
-    vector<int> findOrder(int n, vector<vector<int>>& pre) {
-        vector<vector<int>>graph(n);
-        vector<int>indeg(n,0);
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>ans;
         
-        for(auto p:pre){
+        vector<vector<int>>graph(numCourses);
+        vector<int>count(numCourses,0);
+        
+        for(auto p:prerequisites){
             graph[p[1]].push_back(p[0]);
-            indeg[p[0]]++;
+            count[p[0]]++;
         }
         
-        int cnt= 0;
+        
         queue<int>q;
-        
-        for(int i=0;i<n;i++){
-            if(indeg[i]==0) q.push(i);
-        }
-        
-        vector<int>schedule;
-        while(!q.empty()){
-            int curr = q.front();
-            q.pop();
-            schedule.push_back(curr); 
-            cnt++;
-            
-            for(int nbr:graph[curr]){
-                indeg[nbr]--;
-                if(indeg[nbr]==0) q.push(nbr);
+        for(int i=0;i<numCourses;i++){
+            if(count[i]==0){
+                q.push(i);
             }
         }
         
-        if(cnt!=n) return {};
-        return schedule;
+        while(q.size()){
+            int course = q.front();
+            q.pop();
+            ans.push_back(course);
+            for(int c:graph[course]){
+                count[c]--;
+                if(count[c]==0){
+                    q.push(c);
+                }
+            } 
+        }
+        
+        return ans.size()==numCourses ? ans: vector<int>();
+        
     }
 };
