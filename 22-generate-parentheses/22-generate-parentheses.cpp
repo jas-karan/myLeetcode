e889ea54efbe1572718adf1fe1f36b59cfc998ee
Jaskaran Singh <262jaskaran@gmail.com>
2022-09-12 16:22:59 +0530

    Time: 7 ms (55.48%), Space: 13.8 MB (46.82%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    vector<string>res;
-    
-    void dfs(int o,int c,string curr){
-        if(o==0 && c==0){
-            res.push_back(curr);
+    void helper(string curr, vector<string>&ans,int n,int k){
+        if(k==0 && n==0){
+            ans.push_back(curr);
             return;
         }
         
-        if(o!=0) dfs(o-1,c+1,curr+"(");
-        if(c!=0) dfs(o,c-1,curr+")");
+        if(n>0){
+            helper(curr+'(',ans,n-1,k+1);
+        }
+        if(k>0){
+            helper(curr+')',ans,n,k-1);
+        }
     }
     
     vector<string> generateParenthesis(int n) {
-        dfs(n,0,"");
-        return res;
+        vector<string>ans;
+        helper("",ans,n,0);
+        return ans;
     }
 };
