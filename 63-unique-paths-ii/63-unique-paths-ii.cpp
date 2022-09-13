db6c7c6f53358199cfd99fd955f5476ca0e09000
Jaskaran Singh <262jaskaran@gmail.com>
2022-09-13 09:24:54 +0530

    Time: 7 ms (41.13%), Space: 7.8 MB (45.58%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        int m=grid.size(),n=grid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,0));
-        dp[0][0]=!grid[0][0];
-        for(int i=1;i<n;i++) dp[0][i]=grid[0][i]==1?0:dp[0][i-1];
-        for(int i=1;i<m;i++) dp[i][0]=grid[i][0]==1?0:dp[i-1][0];
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        vector<vector<int>>mem(m,vector<int>(n,0));
+        
+        mem[0][0] = grid[0][0]==1 ? 0:1;
+        for(int j=1;j<n;j++){
+            mem[0][j] = grid[0][j]==1 ? 0 : mem[0][j-1];
+        }
+        for(int i=1;i<m;i++){
+            mem[i][0] = grid[i][0]==1 ? 0 : mem[i-1][0];
+        }
         
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
                 if(grid[i][j]==1) continue;
-                dp[i][j]+=dp[i-1][j]+dp[i][j-1];
+                mem[i][j] = mem[i-1][j] + mem[i][j-1];
             }
         }
         
-        return dp[m-1][n-1];
+        return mem[m-1][n-1];
     }
 };
