d9e4dbbac8abce667f29a2a9f0e07392f636e620
Jaskaran Singh <262jaskaran@gmail.com>
2022-09-13 09:51:24 +0530

    Time: 24 ms (38.81%), Space: 24.6 MB (31.95%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    int memo[101][101];
-    int n,m;
-    
-    int dfs(string s1,int i,string s2,int j,string s3,int k){
-        if(i==n) return s2.substr(j)==s3.substr(k);
-        if(j==m) return s1.substr(i)==s3.substr(k);
+    int mem[101][101];
+    bool dfs(string s1,string s2,string s3,int i,int j,int k){
+        if(i==s1.length()) return s2.substr(j)==s3.substr(k);
+        if(j==s2.length()) return s1.substr(i)==s3.substr(k);
         
-        if(memo[i][j]!=-1) return memo[i][j];
+        if(mem[i][j]!=-1) return mem[i][j];
         
-        if(s1[i]==s3[k] && dfs(s1,i+1,s2,j,s3,k+1) || s2[j]==s3[k] && dfs(s1,i,s2,j+1,s3,k+1))
-            memo[i][j]=1;
-        else memo[i][j]=0;
-        
-        return memo[i][j];
+        bool ans=false;
+        if(i<s1.length() && s1[i]==s3[k]){
+            ans = ans || dfs(s1,s2,s3,i+1,j,k+1);
+        }
+        if(j<s2.length() && s2[j]==s3[k]){
+            ans = ans || dfs(s1,s2,s3,i,j+1,k+1);
+        }
+        return mem[i][j]=ans;
     }
     
     bool isInterleave(string s1, string s2, string s3) {
-        n=s1.length();
-        m=s2.length();
-        if(n+m!=s3.length()) return false;
-        
-        memset(memo,-1,sizeof(memo));
-        return dfs(s1,0,s2,0,s3,0);
+        if(s1.length()+s2.length()!=s3.length()) return false;
+        memset(mem,-1,sizeof(mem));
+        return dfs(s1,s2,s3,0,0,0);
     }
 };
