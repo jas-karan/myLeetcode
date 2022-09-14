4233e40c314a47eaf2b9fa6dc89f26265f325155
Jaskaran Singh <262jaskaran@gmail.com>
2022-09-14 15:47:32 +0530

    Time: 159 ms (22.37%), Space: 11.2 MB (71.23%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int sl = s.length();
-	    int pl = p.length();
-	
-	    vector<vector<bool>>dp(sl+1,vector<bool>(pl+1,false));
-	
-	    dp[0][0] = true;
-	
-	    for(int j=1;j<=pl;j++){
-	    	dp[0][j] = p[j-1]=='*' && dp[0][j-1];
-	    }
-	
-	    for(int j=1;j<=pl;j++){
-		    for(int i=1;i<=sl;i++){
-			    if(p[j-1]==s[i-1] || p[j-1]=='?')
-				    dp[i][j] = dp[i-1][j-1];
-			
-			    else if(p[j-1]=='*'){
-				    dp[i][j] = dp[i][j-1] || dp[i-1][j];
-			    }
-		    }
-	    }
-	
-	    return dp[sl][pl];
+        int a = s.length();
+        int b = p.length();
+        
+        vector<vector<bool>>mem(a+1,vector<bool>(b+1,false));
+        
+        mem[0][0] = true;
+        
+        for(int j=1;j<=b;j++){
+            mem[0][j] = ((p[j-1]=='*') && mem[0][j-1]);
+        }
+        
+        
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=b;j++){
+                if(s[i-1]==p[j-1] || p[j-1]=='?'){
+                    mem[i][j] = mem[i-1][j-1];
+                }
+                else if(p[j-1]=='*'){
+                    mem[i][j] = mem[i-1][j] || mem[i][j-1];
+                }
+            }
+        }
+        
+        return mem[a][b];
     }
 };
