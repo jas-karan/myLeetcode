22eb903f6beaf4608225dbe25402da47b3a8b05e
Jaskaran Singh <262jaskaran@gmail.com>
2022-09-14 10:05:38 +0530

    Time: 29 ms (40.17%), Space: 19.6 MB (99.90%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int trap(vector<int>& h) {
-        //idea - keep maxleft and maxright
-        //so, until curr left bar is less than maxleft, you can add curr left height to ans
-        //similarly for right bars
+    int trap(vector<int>& height) {
+        int n = height.size();
         
-        //see the fig - when left>maxl or right>maxr then we are not adding that height
-        int ans = 0;
-        int i=0,j=h.size()-1;
-        int maxl = 0,maxr = 0;
+        if(n<=2) return 0;
         
-        while(i<=j){
-            if(h[i]<=h[j]){
-                if(h[i]>=maxl) maxl=h[i];
-                else ans+=(maxl-h[i]);
-                i++;
+        int lmax = INT_MIN,rmax = INT_MIN;
+        
+        int l=0,r=n-1;
+        int water=0;
+        while(l<=r){
+            lmax = max(lmax,height[l]);
+            rmax = max(rmax,height[r]);
+            
+            if(height[l]<height[r]){
+                water+=(min(lmax,rmax)-height[l]);
+                l++;
             }
             else{
-                if(h[j]>=maxr) maxr=h[j];
-                else ans+=(maxr-h[j]);
-                j--;
+                water+=(min(lmax,rmax)-height[r]);
+                r--;
             }
         }
         
-        return ans;
+        return water;
     }
 };
