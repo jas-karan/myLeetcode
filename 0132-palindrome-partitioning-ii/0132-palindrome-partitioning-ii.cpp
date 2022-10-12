8c870e3d5bdcdb2e6b7f49035d7c9f3cadcf8a51
Jaskaran Singh <262jaskaran@gmail.com>
2022-10-12 10:54:46 +0530

    Time: 1125 ms (5.02%), Space: 378.3 MB (5.20%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool check(string s,int i,int j){
+        while(i<j) if(s[i++]!=s[j--]) return false;
+        return true;
+    }
+    
+    int dfs(string s,int ind,vector<int>&dp,vector<vector<bool>>&mem){
+        
+        if(ind>=s.length()) return -1;
+        if(dp[ind]!=-1) return dp[ind];
+        int ans = INT_MAX;
+        
+        for(int i=ind;i<s.length();i++){
+            if(s[i]==s[ind]&& (i-ind+1<=2 || mem[ind+1][i-1])){
+                mem[ind][i]=true;
+                ans=min(ans,1+dfs(s,i+1,dp,mem));
+            }
+        }
+        
+        return dp[ind]=ans;
+    }
+    
+    int minCut(string s) {
+        vector<vector<bool>>mem(s.length(),vector<bool>(s.length(),false));
+        vector<int>dp(s.length(),-1);
+        return dfs(s,0,dp,mem);
+    }
+};
