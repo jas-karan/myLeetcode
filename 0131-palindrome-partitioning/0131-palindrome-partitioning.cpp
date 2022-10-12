cd5f3c84369da0ff20e22a2c1885625a5a96dba9
Jaskaran Singh <262jaskaran@gmail.com>
2022-10-12 10:38:47 +0530

    Time: 227 ms (57.38%), Space: 53.5 MB (69.52%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -5,26 +5,28 @@ public:
         return true;
     }
     
-    void dfs(string s,int ind,vector<string>&cur, vector<vector<string>>&res){
+    void dfs(string s,int ind,vector<string>&cur, vector<vector<string>>&res,vector<vector<bool>>&mem){
         if(ind>=s.length()){
             res.push_back(cur);
             return;
         }
         
         for(int i=ind;i<s.length();i++){
-            if(check(s,ind,i)){
+            if(s[i]==s[ind]&& (i-ind+1<=2 || mem[ind+1][i-1])){
+                mem[ind][i]=true;
                 cur.push_back(s.substr(ind,i-ind+1));
-                dfs(s,i+1,cur,res);
+                dfs(s,i+1,cur,res,mem);
                 cur.pop_back();
             }
         }
     }
     
     vector<vector<string>> partition(string s) {
+        vector<vector<bool>>mem(s.length(),vector<bool>(s.length(),false));
         vector<vector<string>>res;
         vector<string>cur;
         
-        dfs(s,0,cur,res);
+        dfs(s,0,cur,res,mem);
         
         return res;
     }
