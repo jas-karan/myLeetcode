3ddfaa20353d88ea8329bba2845ef8394750a234
Jaskaran Singh <262jaskaran@gmail.com>
2022-10-14 13:26:34 +0530

    Time: 789 ms (66.89%), Space: 241 MB (41.43%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -1,20 +1,48 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-template<class T> using oset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
 class Solution {
 public:
-    vector<int> countSmaller(vector<int>& nums) {
-        oset<int>s;
-        int n = nums.size();
-        vector<int>cnt(n,0);
-        
-        for(int i=n-1;i>=0;i--){
-            cnt[i] = s.order_of_key(nums[i]);
-            
-            s.insert(nums[i]);
+    void merge(vector<pair<int,int>>&nums,int l,int m,int r,vector<int>&cnt){
+        vector<pair<int,int>>t(r-l+1);
+        int i=l,j=m+1;
+        int k=0;
+        while(i<=m && j<=r){
+            if(nums[i].first<=nums[j].first){
+                t[k++] = nums[j++];
+            }
+            else{
+                
+                cnt[nums[i].second] += r-j+1;
+                t[k++] = nums[i++];
+            }
         }
+        
+        while(i<=m) t[k++] = nums[i++];
+        while(j<=r) t[k++] = nums[j++];
+        
+        for(i=l;i<=r;i++){
+            nums[i] = t[i-l];
+        }
+        
+    }
+    
+    void mergeSort(vector<pair<int,int>>&nums,int l,int r,vector<int>&cnt){
+        if(l>=r) return;
+        
+        int m = l+(r-l)/2;
+        
+        mergeSort(nums,l,m,cnt);
+        mergeSort(nums,m+1,r,cnt);
+        merge(nums,l,m,r,cnt);
+    }
+    
+    vector<int> countSmaller(vector<int>& nums) {
+        vector<int>cnt(nums.size(),0);
+        vector<pair<int,int>>num(nums.size());
+        
+        for(int i=0;i<nums.size();i++){
+            num[i] = {nums[i],i};
+        }
+        
+        mergeSort(num,0,nums.size()-1,cnt);
         return cnt;
     }
 };
